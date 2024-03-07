93e8f20ae77df1552ec30e35b2dddf6e73c5492f
Kaustubh Ladiya <k4ustu3h@gmail.com>
2024-03-08 02:32:05 +0530

    Sort properties in cssVars

--- src/styles/cssVars.js
+++ src/styles/cssVars.js
@@ -2,60 +2,20 @@ import experimental_extendTheme from "@mui/material/styles/experimental_extendTh
 import pallette from "./pallette.json";
 import { customComponents } from "./customComponents";
 
-const lightPalette = pallette.schemes.light;
 const darkPalette = pallette.schemes.dark;
+const lightPalette = pallette.schemes.light;
 const primaryTones = pallette.palettes.primary;
 const secondaryTones = pallette.palettes.secondary;
 
 export const cssVars = experimental_extendTheme({
 	colorSchemes: {
-		light: {
-			palette: {
-				background: {
-					default: lightPalette.surface,
-				},
-				surface: {
-					container: {
-						inverse: lightPalette.inverseSurface,
-						low: lightPalette.surfaceContainerLow,
-					},
-				},
-				on: {
-					primary: lightPalette.onPrimary,
-				},
-				primary: {
-					container: lightPalette.primaryContainer,
-					inverse: lightPalette.inversePrimary,
-					hover: primaryTones[50],
-					main: lightPalette.primary,
-				},
-				secondary: {
-					container: lightPalette.secondaryContainer,
-					hover: secondaryTones[95],
-					main: lightPalette.secondary,
-					on: {
-						container: lightPalette.onSecondaryContainer,
-					},
-				},
-				tertiary: {
-					container: lightPalette.tertiaryContainer,
-					main: lightPalette.tertiary,
-				},
-				error: {
-					main: lightPalette.error,
-				},
-			},
-		},
 		dark: {
 			palette: {
 				background: {
 					default: darkPalette.surface,
 				},
-				surface: {
-					container: {
-						inverse: darkPalette.inverseSurface,
-						low: darkPalette.surfaceContainerLow,
-					},
+				error: {
+					main: darkPalette.error,
 				},
 				on: {
 					primary: darkPalette.onPrimary,
@@ -74,29 +34,55 @@ export const cssVars = experimental_extendTheme({
 						container: darkPalette.onSecondaryContainer,
 					},
 				},
+				surface: {
+					container: {
+						inverse: darkPalette.inverseSurface,
+						low: darkPalette.surfaceContainerLow,
+					},
+				},
 				tertiary: {
 					container: darkPalette.tertiaryContainer,
 					main: darkPalette.tertiary,
 				},
+			},
+		},
+		light: {
+			palette: {
+				background: {
+					default: lightPalette.surface,
+				},
 				error: {
-					main: darkPalette.error,
+					main: lightPalette.error,
+				},
+				on: {
+					primary: lightPalette.onPrimary,
+				},
+				primary: {
+					container: lightPalette.primaryContainer,
+					hover: primaryTones[50],
+					inverse: lightPalette.inversePrimary,
+					main: lightPalette.primary,
+				},
+				secondary: {
+					container: lightPalette.secondaryContainer,
+					hover: secondaryTones[95],
+					main: lightPalette.secondary,
+					on: {
+						container: lightPalette.onSecondaryContainer,
+					},
+				},
+				surface: {
+					container: {
+						inverse: lightPalette.inverseSurface,
+						low: lightPalette.surfaceContainerLow,
+					},
+				},
+				tertiary: {
+					container: lightPalette.tertiaryContainer,
+					main: lightPalette.tertiary,
 				},
 			},
 		},
-	},
-	typography: {
-		fontFamily: "Outfit",
-		logo: {
-			fontFamily: "Afacad",
-			fontSize: 48,
-			fontStyle: "italic",
-			fontWeight: "bold",
-			userSelect: "none",
-		},
-		h3: {
-			fontWeight: "bold",
-		},
-		clock: customComponents.typography.clock,
 	},
 	components: {
 		MuiButton: {
@@ -104,52 +90,52 @@ export const cssVars = experimental_extendTheme({
 				{
 					props: { variant: "filled" },
 					style: ({ theme }) => ({
-						height: 40,
+						backgroundColor: theme.palette.primary.main,
+						borderRadius: 20,
 						color: theme.palette.on.primary,
+						height: 40,
 						paddingLeft: 24,
 						paddingRight: 24,
-						borderRadius: 20,
-						backgroundColor: theme.palette.primary.main,
+						textTransform: "none",
 						":hover": {
 							backgroundColor: theme.palette.primary.hover,
 						},
-						textTransform: "none",
 					}),
+				},
+				{
+					props: { variant: "outlined" },
+					style: {
+						borderRadius: 20,
+						height: 40,
+						paddingLeft: 24,
+						paddingRight: 24,
+						textTransform: "none",
+					},
 				},
 				{
 					props: { variant: "text" },
 					style: {
+						borderRadius: 20,
 						height: 40,
 						paddingLeft: 12,
 						paddingRight: 12,
-						borderRadius: 20,
 						textTransform: "none",
 					},
 				},
 				{
 					props: { variant: "tonal" },
 					style: ({ theme }) => ({
-						height: 40,
+						backgroundColor: theme.palette.secondary.container,
+						borderRadius: 20,
 						color: theme.palette.secondary.on.container,
+						height: 40,
 						paddingLeft: 24,
 						paddingRight: 24,
-						borderRadius: 20,
-						backgroundColor: theme.palette.secondary.container,
+						textTransform: "none",
 						":hover": {
 							backgroundColor: theme.palette.secondary.hover,
 						},
-						textTransform: "none",
 					}),
-				},
-				{
-					props: { variant: "outlined" },
-					style: {
-						height: 40,
-						paddingLeft: 24,
-						paddingRight: 24,
-						borderRadius: 20,
-						textTransform: "none",
-					},
 				},
 			],
 		},
@@ -163,6 +149,20 @@ export const cssVars = experimental_extendTheme({
 					}),
 				},
 			],
+		},
+	},
+	typography: {
+		clock: customComponents.typography.clock,
+		fontFamily: "Outfit",
+		h3: {
+			fontWeight: "bold",
+		},
+		logo: {
+			fontFamily: "Afacad",
+			fontSize: 48,
+			fontStyle: "italic",
+			fontWeight: "bold",
+			userSelect: "none",
 		},
 	},
 });

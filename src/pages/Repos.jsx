99af061c2be8e96a5e33cab12c24419177117da0
Kaustubh Ladiya <k4ustu3h@gmail.com>
2024-03-19 23:32:17 +0530

    Remove unnecessary check for 0 repositories

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -36,49 +36,45 @@ export default function Repos() {
 			<Container
 				sx={{ alignContent: "center", color: "on.surface", p: 2 }}
 			>
-				{repos.sourceRepos.length > 0 && (
-					<Container>
-						<Typography py={4} variant="h4">
-							Source Repositories
-						</Typography>
-						<Grid container spacing={4}>
-							{repos.sourceRepos.map((repo) => (
-								<Grid
-									key={repo.id}
-									item
-									xs={12}
-									sm={8}
-									md={6}
-									lg={4}
-								>
-									<RepoCard repo={repo} />
-								</Grid>
-							))}
-						</Grid>
-					</Container>
-				)}
+				<Container>
+					<Typography py={4} variant="h4">
+						Source Repositories
+					</Typography>
+					<Grid container spacing={4}>
+						{repos.sourceRepos.map((repo) => (
+							<Grid
+								key={repo.id}
+								item
+								xs={12}
+								sm={8}
+								md={6}
+								lg={4}
+							>
+								<RepoCard repo={repo} />
+							</Grid>
+						))}
+					</Grid>
+				</Container>
 				<Divider sx={{ py: 4 }} variant="middle" />
-				{repos.forkedRepos.length > 0 && (
-					<Container sx={{ py: 4 }}>
-						<Typography py={4} variant="h4">
-							Forked Repositories
-						</Typography>
-						<Grid container spacing={4}>
-							{repos.forkedRepos.map((repo) => (
-								<Grid
-									key={repo.id}
-									item
-									xs={12}
-									sm={8}
-									md={6}
-									lg={4}
-								>
-									<RepoCard repo={repo} />
-								</Grid>
-							))}
-						</Grid>
-					</Container>
-				)}
+				<Container sx={{ py: 4 }}>
+					<Typography py={4} variant="h4">
+						Forked Repositories
+					</Typography>
+					<Grid container spacing={4}>
+						{repos.forkedRepos.map((repo) => (
+							<Grid
+								key={repo.id}
+								item
+								xs={12}
+								sm={8}
+								md={6}
+								lg={4}
+							>
+								<RepoCard repo={repo} />
+							</Grid>
+						))}
+					</Grid>
+				</Container>
 			</Container>
 			<Footer />
 		</Experimental_CssVarsProvider>

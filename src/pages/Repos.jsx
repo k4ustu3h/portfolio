244fcc64753952538abff5db19bac94eb4735152
Kaustubh Ladiya <k4ustu3h@gmail.com>
2024-03-20 03:08:47 +0530

    Add Skeletons for RepoCard

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Experimental_CssVarsProvider } from "@mui/material";
-import Divider from "@mui/material/Divider";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { cssVars } from "../styles/cssVars.js";
 import Footer from "../components/sections/Footer.jsx";
 import NavBar from "../components/surfaces/NavBar.jsx";
 import RepoCard from "../components/cards/RepoCard";
+import RepoCardSkeleton from "../components/feedback/RepoCardSkeleton.jsx";
 
 export default function Repos() {
 	const [repos, setRepos] = useState({ forkedRepos: [], sourceRepos: [] });
@@ -41,18 +42,22 @@ export default function Repos() {
 						Source Repositories
 					</Typography>
 					<Grid container spacing={4}>
-						{repos.sourceRepos.map((repo) => (
-							<Grid
-								key={repo.id}
-								item
-								xs={12}
-								sm={8}
-								md={6}
-								lg={4}
-							>
-								<RepoCard repo={repo} />
-							</Grid>
-						))}
+						{repos.sourceRepos.length === 0 ? (
+							<RepoCardSkeleton />
+						) : (
+							repos.sourceRepos.map((repo) => (
+								<Grid
+									key={repo.id}
+									item
+									xs={12}
+									sm={8}
+									md={6}
+									lg={4}
+								>
+									<RepoCard repo={repo} />
+								</Grid>
+							))
+						)}
 					</Grid>
 				</Container>
 				<Divider sx={{ py: 4 }} variant="middle" />
@@ -61,18 +66,22 @@ export default function Repos() {
 						Forked Repositories
 					</Typography>
 					<Grid container spacing={4}>
-						{repos.forkedRepos.map((repo) => (
-							<Grid
-								key={repo.id}
-								item
-								xs={12}
-								sm={8}
-								md={6}
-								lg={4}
-							>
-								<RepoCard repo={repo} />
-							</Grid>
-						))}
+						{repos.forkedRepos.length === 0 ? (
+							<RepoCardSkeleton />
+						) : (
+							repos.forkedRepos.map((repo) => (
+								<Grid
+									key={repo.id}
+									item
+									xs={12}
+									sm={8}
+									md={6}
+									lg={4}
+								>
+									<RepoCard repo={repo} />
+								</Grid>
+							))
+						)}
 					</Grid>
 				</Container>
 			</Container>

9c3584d5bc2f3d45d32f1b081f42e956d4285b9a
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-07-30 19:14:03 +0530

    Enclose app in M3eTheme

--- src/components/ThemeWrapper.jsx
+++ src/components/ThemeWrapper.jsx
@@ -4,6 +4,8 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import { M3eTheme } from "@m3e/react/theme";
+
 import WallpaperProvider from "@/contexts/WallpaperContext";
 
 import { getCSSVars } from "@/styles/cssVars";
@@ -11,12 +13,14 @@ import { getCSSVars } from "@/styles/cssVars";
 export default function ThemeWrapper({ children, theme, wallpaper }) {
 	return (
 		<AppRouterCacheProvider>
-			<WallpaperProvider value={wallpaper}>
-				<ThemeProvider theme={getCSSVars(theme)}>
-					<CssBaseline />
-					{children}
-				</ThemeProvider>
-			</WallpaperProvider>
+			<M3eTheme color={theme.schemes.light.primary} variant="expressive">
+				<WallpaperProvider value={wallpaper}>
+					<ThemeProvider theme={getCSSVars(theme)}>
+						<CssBaseline />
+						{children}
+					</ThemeProvider>
+				</WallpaperProvider>
+			</M3eTheme>
 		</AppRouterCacheProvider>
 	);
 }

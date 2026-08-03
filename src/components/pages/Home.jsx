4054e37684e6e2c250c26ee991ed353753e32ffd
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-08-03 15:08:48 +0530

    Fix SSR in Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,10 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const About = dynamic(() => import("@/components/layout/About.jsx"), {
-	ssr: false,
-});
-const Projects = dynamic(() => import("@/components/layout/Projects.jsx"), {
-	ssr: false,
-});
-
+import About from "@/components/layout/About.jsx";
+import Projects from "@/components/layout/Projects.jsx";
 import Footer from "@/components/layout/Footer.jsx";
 import Hero from "@/components/layout/Hero.jsx";
+
 import NavBar from "@/components/surfaces/NavBar.jsx";
 
 export default function Home() {
